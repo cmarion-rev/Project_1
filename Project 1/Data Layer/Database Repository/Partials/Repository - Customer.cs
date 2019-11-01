97bb4a7e50bafbd3dabae87146e8925159e6c17f
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-01 11:17:42 -0400

    Created new partial class for Repository to work with customer information.

---
+++ Project 1/Data Layer/Database Repository/Partials/Repository - Customer.cs
@@ -0,0 +1,48 @@
+﻿using Data_Layer.Data_Objects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Layer
+{
+    public partial class Repository
+    {
+        public async Task<Customer> GetCustomer(int id)
+        {
+            Customer result = null;
+
+            try
+            {
+                result = await myContext.Customers.Where(c => c.ID == id).FirstOrDefaultAsync();
+            }
+            catch (Exception WTF)
+            {
+                Console.WriteLine(WTF);
+                throw;
+            }
+            finally
+            {
+
+            }
+            return result;
+        }
+
+        public async Task<List<Customer>> GetAllCustomers()
+        {
+            List<Customer> results = await myContext.Customers.ToListAsync();
+
+            return results;
+        }
+
+        public async Task<Customer> UpdateCustomer(Customer currentCustomer)
+        {
+            myContext.Update(currentCustomer);
+            await myContext.SaveChangesAsync();
+
+            return currentCustomer;
+        }
+    }
+}
