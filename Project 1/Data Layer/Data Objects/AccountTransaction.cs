f4e7594fe52ab7bd55d4fa9507c934e2c33210c0
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-01 09:49:11 -0400

    Created new property class for tracking account transactions.

---
+++ Project 1/Data Layer/Data Objects/AccountTransaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Layer.Data_Objects
+{
+    public class AccountTransaction
+    {
+        public int ID { get; set; }
+
+        public int AccountID { get; set; }
+
+        public virtual Account Account { get; set; }
+
+        public int TransactionCode { get; set; }
+
+        public double Amount { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+    }
+}
