780e32ccdf7c4236b307a70bdca884b2be7a63bc
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-01 09:41:50 -0400

    Updated Account class for new Customer class.

--- Project 1/Data Layer/Data Objects/Account.cs
+++ Project 1/Data Layer/Data Objects/Account.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Data_Layer.Data_Objects
@@ -10,16 +11,24 @@ namespace Data_Layer.Data_Objects
 
         public int AccountTypeID { get; set; }
 
+        [Display(Name = "Account Type")]
         public virtual AccountType AccountType { get; set; }
 
         public int CustomerID { get; set; }
 
+        public virtual Customer Customer { get; set; }
+
+        [Display(Name = "Balance")]
+        [DataType(DataType.Currency)]
         public double AccountBalance { get; set; }
 
+        [Display(Name = "Last Transaction State")]
         public int LastTransactionState { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime MaturityDate { get; set; }
 
+        [Display(Name = "Interest Rate")]
         public float InterestRate { get; set; }
 
         public bool IsActive { get; set; }
