c3bc30cb3e09ff3c770e26a5dbd677bb19f6d5a1
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-01 10:21:41 -0400

    Created new class for acting as repository facade to web interface.

---
+++ Project 1/Data Layer/Database Repository/Repository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Layer
+{
+    public class Repository
+    {
+        private MainDbContext myContext = null;
+
+        public Repository(MainDbContext newContext)
+        {
+            myContext = newContext;
+        }
+
+
+
+    }
+}
