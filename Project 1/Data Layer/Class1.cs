3263cf277734cdfac2311321d2481deee76cce72
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-01 09:09:31 -0400

    Updated connection string for DbContext class.

--- Project 1/Data Layer/Class1.cs
+++ Project 1/Data Layer/Class1.cs
@@ -7,15 +7,7 @@ namespace Data_Layer
     {
         public DbSet<Customer> Customers { get; set; }
 
-        public DbSet<Order> Orders { get; set; }
-
-        public DbSet<State> States { get; set; }
-
-        public DbSet<OrderDetail> OrderDetails { get; set; }
-
-        public DbSet<Product> Products { get; set; }
-
-
+      
         public MyDbContext(DbContextOptions<MyDbContext> context) : base(context)
         {
 
@@ -24,14 +16,14 @@ namespace Data_Layer
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<OrderDetail>().HasKey(t => new { t.OrderID, t.ProductID });
+            //modelBuilder.Entity<OrderDetail>().HasKey(t => new { t.OrderID, t.ProductID });
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=TestOne;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=Project1;Trusted_Connection=True;");
             }
         }
 
