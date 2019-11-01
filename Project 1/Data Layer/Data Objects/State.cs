8f6af211487a0407b819568978012d2a77800d15
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-01 09:44:53 -0400

    Created new property class for listing State names and/or abbreviations.

---
+++ Project 1/Data Layer/Data Objects/State.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Data_Layer.Data_Objects
+{
+    public class State
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(2)]
+        public string Abbreviation { get; set; }
+    }
+}
