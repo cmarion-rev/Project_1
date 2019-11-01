ee96a8607fa735cd636f959eb06522b52c5ece28
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-01 09:16:54 -0400

    Created a new Account property class.

---
+++ Project 1/Data Layer/Data Objects/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Layer.Data_Objects
+{
+    class Account
+    {
+        public int ID { get; set; }
+
+        public int AccountTypeID { get; set; }
+
+        public int CustomerID { get; set; }
+
+        public double AccountBalance { get; set; }
+
+        public int LastTransactionState { get; set; }
+
+        public DateTime MaturityDate { get; set; }
+
+        public float InterestRate { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public bool IsOpen { get; set; }
+    }
+}
