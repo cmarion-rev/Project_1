58be767cd44d09ed928bdef378ba075ca729c869
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-01 09:03:49 -0400

    Initial setup of authentication web app connection to database.

--- Project 1/Web Interface/Data/Migrations/20191101130058_base.Designer.cs
+++ Project 1/Web Interface/Data/Migrations/20191101130058_base.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Web_Interface.Data;
 
 namespace Web_Interface.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191101130058_base")]
+    partial class @base
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
