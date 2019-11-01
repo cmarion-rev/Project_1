f792b248c390cb4dfb034997fb55da2589e8383b
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-01 14:51:55 -0400

    Created new custom exception for preventing withdrawing from non-matured account.

---
+++ Project 1/Data Layer/Errors/MaturityValidationException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Layer.Errors
+{
+    [Serializable]
+    public class MaturityValidationException : Exception
+    {
+        public MaturityValidationException() { }
+       
+        public MaturityValidationException(string message) : base(message) { }
+        
+        public MaturityValidationException(string message, Exception inner) : base(message, inner) { }
+        
+        protected MaturityValidationException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
