82c1814518e6032d7059cc36845592c9104ecbd5
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-01 15:28:59 -0400

    Added new overload to GetAllTransactions for limiting return results by specific count.

--- Project 1/Data Layer/Database Repository/Partials/Repository - Account Transactions.cs
+++ Project 1/Data Layer/Database Repository/Partials/Repository - Account Transactions.cs
@@ -23,7 +23,9 @@ namespace Data_Layer
                 // Check if owning customer.
                 if (tempAccount.CustomerID == tempCustomer.ID)
                 {
-                    List<AccountTransaction> tempTransactions = await myContext.AccountTransactions.Where(t => t.AccountID == accountID).OrderBy(t => t.TimeStamp).ToListAsync();
+                    List<AccountTransaction> tempTransactions = await myContext.AccountTransactions.
+                                                                        Where(t => t.AccountID == accountID).
+                                                                        OrderByDescending(t => t.TimeStamp).ToListAsync();
                     
                     // Prepare view model for return.
                     result.Customer = tempCustomer;
@@ -54,7 +56,7 @@ namespace Data_Layer
             return result;
         }
 
-        public async Task<CustomerAccountTransactionsVM> GetAllTransactionsByDate(int customerID, int accountID, DateTime startDate, DateTime endDate)
+        public async Task<CustomerAccountTransactionsVM> GetAllTransactions(int customerID, int accountID, DateTime startDate, DateTime endDate)
         {
             CustomerAccountTransactionsVM result = new CustomerAccountTransactionsVM();
 
@@ -101,5 +103,52 @@ namespace Data_Layer
 
             return result;
         }
+
+        public async Task<CustomerAccountTransactionsVM> GetAllTransactions(int customerID, int accountID, int resultLimit)
+        {
+            CustomerAccountTransactionsVM result = new CustomerAccountTransactionsVM();
+
+            try
+            {
+                Customer tempCustomer = await myContext.Customers.Where(c => c.ID == customerID).FirstOrDefaultAsync();
+                Account tempAccount = await myContext.Accounts.Where(a => a.ID == accountID).FirstOrDefaultAsync();
+
+                // Check if owning customer.
+                if (tempAccount.CustomerID == tempCustomer.ID)
+                {
+                    List<AccountTransaction> tempTransactions = await myContext.AccountTransactions.
+                                                                        Where(t => t.AccountID == accountID).
+                                                                        OrderByDescending(t => t.TimeStamp).
+                                                                        Take(resultLimit).ToListAsync();
+
+                    // Prepare view model for return.
+                    result.Customer = tempCustomer;
+                    result.Account = tempAccount;
+                    result.AccountTransactions = tempTransactions;
+                }
+                else
+                {
+                    // Unauthorized user attempting to access an account not their's.
+                    throw new UnauthorizedAccessException(string.Format("CUSTOMER #{0} DOES NOT HAVE ACCESS TO ACCOUNT #{1}", customerID, accountID));
+                }
+            }
+            catch (UnauthorizedAccessException WTF)
+            {
+                Console.WriteLine(WTF);
+                throw;
+            }
+            catch (Exception WTF)
+            {
+                Console.WriteLine(WTF);
+                throw;
+            }
+            finally
+            {
+
+            }
+
+            return result;
+        }
+
     }
 }
