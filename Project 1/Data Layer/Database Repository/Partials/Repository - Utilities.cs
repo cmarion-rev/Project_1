6ff98fbece7833b2b055594ee30b93351b82d519
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-01 10:33:22 -0400

    Created new Utilites partial file for Repository class to contain miscellaneous functions.

---
+++ Project 1/Data Layer/Database Repository/Partials/Repository - Utilities.cs
@@ -0,0 +1,42 @@
+﻿using Data_Layer.Data_Objects;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Layer
+{
+    public partial class Repository
+    {
+        public async Task<List<State>> GetStates()
+        {
+            List<State> results = await myContext.States.ToListAsync();
+
+            return results;
+        }
+
+        public async Task<State> GetState(int ID)
+        {
+            State result = null;
+            
+            try
+            {
+                result = await myContext.States.Where(s => s.ID == ID).FirstOrDefaultAsync();
+            }
+            catch (Exception WTF)
+            {
+                // Log error.
+                Console.WriteLine(WTF);
+                throw;
+            }
+            finally
+            {
+            
+            }
+
+            return result;
+        }
+    }
+}
