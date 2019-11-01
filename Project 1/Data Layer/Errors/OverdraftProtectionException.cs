287b17b9ebb34f154381b0740ed9f24bd394cbc1
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-01 14:50:58 -0400

    Created new custom exception for identifying withdrawal overdraft protection activation.

---
+++ Project 1/Data Layer/Errors/OverdraftProtectionException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Layer.Errors
+{
+    [System.Serializable]
+    public class OverdraftProtectionException : Exception
+    {
+        public OverdraftProtectionException() { }
+        
+        public OverdraftProtectionException(string message) : base(message) { }
+        
+        public OverdraftProtectionException(string message, Exception inner) : base(message, inner) { }
+        
+        protected OverdraftProtectionException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
