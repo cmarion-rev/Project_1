48ce61a18d84a6a26d7c686b789c13c067b4b2b9
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-01 09:15:28 -0400

    Created new resource class Utility for containing all relevant static resource items.

---
+++ Project 1/Data Layer/Resources/Utility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Layer.Resources
+{
+    static class Utility
+    {
+        public enum AccountType
+        {
+            INVALID_ACCOUNT =-1,
+
+            CHECKING,
+            BUSINESS,
+            TERM_DEPOSIT,
+            LOAN,
+
+
+
+
+            _COUNT,
+        }
+
+        public enum TransactionCodes
+        {
+            DEPOSIT,
+            WITHDRAWAL,
+
+            // ERRORS
+            OVERDRAFT_PROTECTION,
+            NON_MATURITY,
+
+
+        }
+    }
+}
