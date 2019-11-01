e4243ddb8c05e22e18b38bce91b69c3d3ef37d32
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-01 15:30:33 -0400

    Added in overload for GetAllTransactions function to limit results by date and return specified count.

--- Project 1/Data Layer/Database Repository/Partials/Repository - Account Transactions.cs
+++ Project 1/Data Layer/Database Repository/Partials/Repository - Account Transactions.cs
@@ -73,7 +73,7 @@ namespace Data_Layer
                                                                         Where(t => t.AccountID == accountID && 
                                                                               startDate <= t.TimeStamp && 
                                                                               t.TimeStamp <= endDate).
-                                                                        OrderBy(t => t.TimeStamp).ToListAsync();
+                                                                        OrderByDescending(t => t.TimeStamp).ToListAsync();
 
                     // Prepare view model for return.
                     result.Customer = tempCustomer;
@@ -150,5 +150,53 @@ namespace Data_Layer
             return result;
         }
 
+        public async Task<CustomerAccountTransactionsVM> GetAllTransactions(int customerID, int accountID, DateTime startDate, DateTime endDate, int resultLimit)
+        {
+            CustomerAccountTransactionsVM result = new CustomerAccountTransactionsVM();
+
+            try
+            {
+                Customer tempCustomer = await myContext.Customers.Where(c => c.ID == customerID).FirstOrDefaultAsync();
+                Account tempAccount = await myContext.Accounts.Where(a => a.ID == accountID).FirstOrDefaultAsync();
+
+                // Check if owning customer.
+                if (tempAccount.CustomerID == tempCustomer.ID)
+                {
+                    // Limit return transaction list to specified period.
+                    List<AccountTransaction> tempTransactions = await myContext.AccountTransactions.
+                                                                        Where(t => t.AccountID == accountID &&
+                                                                              startDate <= t.TimeStamp &&
+                                                                              t.TimeStamp <= endDate).
+                                                                        OrderByDescending(t => t.TimeStamp).
+                                                                        Take(resultLimit).ToListAsync();
+
+                    // Prepare view model for return.
+                    result.Customer = tempCustomer;
+                    result.Account = tempAccount;
+                    result.AccountTransactions = tempTransactions;
+                }
+                else
+                {
+                    // Unauthorized user attempting to access an account not their's.
+                    throw new UnauthorizedAccessException(string.Format("CUSTOMER #{0} DOES NOT HAVE ACCESS TO ACCOUNT #{1}", customerID, accountID));
+                }
+            }
+            catch (UnauthorizedAccessException WTF)
+            {
+                Console.WriteLine(WTF);
+                throw;
+            }
+            catch (Exception WTF)
+            {
+                Console.WriteLine(WTF);
+                throw;
+            }
+            finally
+            {
+
+            }
+
+            return result;
+        }
     }
 }
