f165955d3c32256f936aa1c410c0fd0163f3040a
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-01 09:08:38 -0400

    Added EntityFramework functionality packages to Data Layer project for use with database.

--- Project 1/Data Layer/Class1.cs
+++ Project 1/Data Layer/Class1.cs
@@ -1,8 +1,39 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Data_Layer
 {
-    public class Class1
+    public class MyDbContext : DbContext
     {
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<State> States { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+
+        public MyDbContext(DbContextOptions<MyDbContext> context) : base(context)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OrderDetail>().HasKey(t => new { t.OrderID, t.ProductID });
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=TestOne;Trusted_Connection=True;");
+            }
+        }
+
     }
 }
