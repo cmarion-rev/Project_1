eac1bb16c0ce34373a079e3a8a6720b1182be12e
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-04 19:16:48 -0500

    Created new interface for Utilities side of Repository class.

---
+++ Project 1/Data Layer/Database Repository/Interfaces/IRepository_Utilties.cs
@@ -0,0 +1,28 @@
+﻿using Data_Layer.Data_Objects;
+using Data_Layer.Resources;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Layer.Database_Repository.Interfaces
+{
+    public interface IRepository_Utilities
+    {
+        public List<State> GetStates();
+
+        public Task<State> GetState(int ID);
+
+        public Task<List<AccountTransactionState>> GetTransactionStates();
+
+        public Task<AccountTransactionState> GetTransactionState(int ID);
+
+        public Task<string> GetAccountTypeName(int id);
+
+        public Task<AccountType> GetAccountType(int id);
+
+        public int GetAccountTypeID(Utility.AccountType accountType);
+
+        public Task<List<AccountType>> GetAllAccountTypes();
+    }
+}
