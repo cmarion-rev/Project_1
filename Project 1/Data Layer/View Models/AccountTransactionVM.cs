d21649fa52b61a2de1a4b6bfc9b19ec9dfd10950
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-05 15:53:07 -0500

    Created new view model for account transactions.

---
+++ Project 1/Data Layer/View Models/AccountTransactionVM.cs
@@ -0,0 +1,16 @@
+﻿using Data_Layer.Data_Objects;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Data_Layer.View_Models
+{
+    public class AccountTransactionVM
+    {
+        public Account Account { get; set; }
+
+        [Range(0.01, double.MaxValue)]
+        public double Amount { get; set; }
+    }
+}
