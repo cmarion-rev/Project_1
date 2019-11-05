ef51ea3a4271229bbde69a59033ad826d63a02ad
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-05 18:01:38 -0500

    Added text display field for Amount property of AccountTransactionVM.

--- Project 1/Data Layer/View Models/AccountTransactionVM.cs
+++ Project 1/Data Layer/View Models/AccountTransactionVM.cs
@@ -10,6 +10,7 @@ namespace Data_Layer.View_Models
     {
         public Account Account { get; set; }
 
+        [Display(Name = "Transaction Amount")]
         [Range(0.01, double.MaxValue)]
         public double Amount { get; set; }
     }
