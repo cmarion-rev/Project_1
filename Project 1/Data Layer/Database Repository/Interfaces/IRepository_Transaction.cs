0454fbbf86dc3d2367674d0fa09d291b60fa67bc
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-06 12:33:05 -0500

    Removed async/await from Transactions side of Repository class.

--- Project 1/Data Layer/Database Repository/Interfaces/IRepository_Transaction.cs
+++ Project 1/Data Layer/Database Repository/Interfaces/IRepository_Transaction.cs
@@ -8,12 +8,12 @@ namespace Data_Layer.Database_Repository.Interfaces
 {
     public interface IRepository_Transaction
     {
-        Task<CustomerAccountTransactionsVM> GetAllTransactions(int customerID, int accountID);
+        public CustomerAccountTransactionsVM GetAllTransactions(int customerID, int accountID);
 
-        Task<CustomerAccountTransactionsVM> GetAllTransactions(int customerID, int accountID, DateTime startDate, DateTime endDate);
+        public CustomerAccountTransactionsVM GetAllTransactions(int customerID, int accountID, DateTime startDate, DateTime endDate);
 
-        Task<CustomerAccountTransactionsVM> GetAllTransactions(int customerID, int accountID, int resultLimit);
+        public CustomerAccountTransactionsVM GetAllTransactions(int customerID, int accountID, int resultLimit);
 
-        Task<CustomerAccountTransactionsVM> GetAllTransactions(int customerID, int accountID, DateTime startDate, DateTime endDate, int resultLimit);
+        public CustomerAccountTransactionsVM GetAllTransactions(int customerID, int accountID, DateTime startDate, DateTime endDate, int resultLimit);
     }
 }
