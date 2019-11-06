c4b9b24967276fa025608f44c061f55373be0426
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-06 16:15:33 -0500

    Created new view model for account transfers.

---
+++ Project 1/Data Layer/View Models/AccountTransferVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Layer.View_Models
+{
+    public class AccountTransferVM
+    {
+        public int SourceID { get; set; }
+
+        public int DestinationID { get; set; }
+
+        public double Amount { get; set; }
+    }
+}
