08e4b0f322c2c8f714f9416780346170cac511eb
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-07 15:10:52 -0500

    Created new database Code First setup and migration of new AccountInterestRates and seeding.

---
+++ Project 1/Data Layer/Migrations/20191107200738_AddInterestRate.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data_Layer.Migrations
+{
+    public partial class AddInterestRate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AccountInterestRates",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountTypeID = table.Column<int>(nullable: false),
+                    Rate = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountInterestRates", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_AccountInterestRates_AccountTypes_AccountTypeID",
+                        column: x => x.AccountTypeID,
+                        principalTable: "AccountTypes",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountInterestRates_AccountTypeID",
+                table: "AccountInterestRates",
+                column: "AccountTypeID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AccountInterestRates");
+        }
+    }
+}
