2c04ad8b20ec8c2736e2e0d86c81025625495765
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-06 19:30:07 -0500

    Minor code cleanup.

--- Project 1/Web Interface/Controllers/CustomersController.cs
+++ Project 1/Web Interface/Controllers/CustomersController.cs
@@ -5,11 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
 using Data_Layer;
 using Data_Layer.Data_Objects;
 using Data_Layer.Database_Repository.Interfaces;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Authorization;
 using Data_Layer.View_Models;
 
 namespace Web_Interface.Controllers
