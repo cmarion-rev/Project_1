223fbbeb47ab0dce948e3dda805e730a0b1df8a4
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-07 10:18:36 -0500

    Used sample code from https://www.jerriepelser.com/blog/unit-testing-controllers-aspnet5/ to create workarounds for session user data to start testing CustomersController.

--- Project 1/UnitTests/CustomerTest.cs
+++ Project 1/UnitTests/CustomerTest.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System;
+using System.Security.Claims;
+using UnitTests.Objects;
+using UnitTests.Repositories;
+using Web_Interface.Controllers;
 
 namespace UnitTests
 {
@@ -8,6 +18,35 @@ namespace UnitTests
         [TestMethod]
         public void TestMethod1()
         {
+            TestRepository testRepo = new TestRepository();
+            CustomersController testController = new CustomersController(testRepo);
+
+
+            var validPrincipal = new ClaimsPrincipal(
+                new[]
+                {
+                     new ClaimsIdentity(
+                         new[] {new Claim(ClaimTypes.NameIdentifier, "UserA")})
+                });
+
+            var httpContext = Substitute.For<HttpContext>();
+            httpContext.User.Returns(validPrincipal);
+            var contContext = Substitute.For<ControllerContext>();
+            contContext.HttpContext = httpContext;
+
+
+            testController = new CustomersController(testRepo)
+            {
+              ControllerContext = contContext,   
+                 //HttpContext = httpContext,
+            };
+
+            var returnUser = testController.User;
+
+
+
+            testController.Create();
+
         }
     }
 }
