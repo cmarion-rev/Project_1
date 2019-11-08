5a5d488d1f633601addfe39891bc33d46ac146df
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-08 11:30:03 -0500

    Added unit test for valid account in Installment Get action of AccountsController.

--- Project 1/UnitTests/AccountTest.cs
+++ Project 1/UnitTests/AccountTest.cs
@@ -17,6 +17,7 @@ using Data_Layer.Data_Objects;
 using Data_Layer.View_Models;
 using Web_Interface.Controllers;
 using UnitTests.Repositories;
+using Data_Layer.Resources;
 
 namespace UnitTests
 {
@@ -1732,7 +1733,66 @@ namespace UnitTests
             #endregion
         }
 
+        [TestMethod]
+        public void TestInstallmentGet_ValidAccount()
+        {
+            #region ASSIGN
 
+            TestRepository tRepo = new TestRepository();
+            AccountsController tController = null;
+
+            #region DO NOT DELETE - FOR GENERATING FAKE SESSION USER DATA
+
+            var validPrincipal = new ClaimsPrincipal(
+                new[]
+                {
+                     new ClaimsIdentity(
+                         new[] {new Claim(ClaimTypes.NameIdentifier, "UserB")})
+                });
+
+            var serviceProviderMock = new Mock<IServiceProvider>();
+            var tempDataFactoryMock = new Mock<ITempDataDictionaryFactory>();
+            var UrlFactoryMock = new Mock<IUrlHelperFactory>();
+            serviceProviderMock
+                .Setup(s => s.GetService(typeof(ITempDataDictionaryFactory)))
+                .Returns(tempDataFactoryMock.Object);
+            serviceProviderMock
+                .Setup(s => s.GetService(typeof(IUrlHelperFactory)))
+                .Returns(UrlFactoryMock.Object);
+
+
+            var httpContext = Substitute.For<HttpContext>();
+            httpContext.RequestServices = serviceProviderMock.Object;
+            httpContext.User.Returns(validPrincipal);
+
+            var contContext = Substitute.For<ControllerContext>();
+            contContext.HttpContext = httpContext;
+
+
+            tController = new AccountsController(tRepo)
+            {
+                ControllerContext = contContext,
+            };
+
+            #endregion
+
+            #endregion
+
+            #region ACT
+
+            var tResult = tController.Installment(3);
+
+            #endregion
+
+            #region ASSERT
+
+            Assert.IsTrue(tResult is ViewResult);
+            Assert.AreEqual(((tResult as ViewResult).Model as AccountTransactionVM).Account.ID, 3);
+            Assert.AreEqual(((tResult as ViewResult).Model as AccountTransactionVM).Account.AccountTypeID, (int)Utility.AccountType.LOAN);
+            Assert.AreEqual(((tResult as ViewResult).Model as AccountTransactionVM).Amount, 0.0);
+
+            #endregion
+        }
         #endregion
     }
 }
