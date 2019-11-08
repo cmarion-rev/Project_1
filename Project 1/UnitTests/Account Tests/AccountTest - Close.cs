a403eb7504f12c42ba82e41bef178702f233d73a
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-08 16:03:11 -0500

    Created new sub-namespace for AccountsController test classes. Cleaned up unneeded Usings in AccountsController test classes.

--- Project 1/UnitTests/Account Tests/AccountTest - Close.cs
+++ Project 1/UnitTests/Account Tests/AccountTest - Close.cs
@@ -1,26 +1,10 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
-using NSubstitute;
-using System;
-using System.Linq;
-using System.Web;
-using System.Security.Claims;
-
-using Data_Layer.Data_Objects;
-using Data_Layer.View_Models;
 using Web_Interface.Controllers;
 using UnitTests.Repositories;
-using Data_Layer.Resources;
 using UnitTests.Resources;
 
-namespace UnitTests
+namespace UnitTests.Account
 {
     [TestClass]
     public class AccountTest_Close
