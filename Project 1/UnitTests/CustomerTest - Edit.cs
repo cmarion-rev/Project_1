6da1f00c1fedace7b19c6bfbe90ba7c6eae71463
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-08 14:23:30 -0500

    Refactored apart unit tests for Index Get action and Edit Get and Post actions in CustomersController, into two separate test classes.

--- Project 1/UnitTests/CustomerTest - Edit.cs
+++ Project 1/UnitTests/CustomerTest - Edit.cs
@@ -22,67 +22,8 @@ using UnitTests.Resources;
 namespace UnitTests
 {
     [TestClass]
-    public class CustomerTest
+    public class CustomerTest_Edit
     {
-        [TestMethod]
-        public void TestIndexGet_NewCustomer()
-        {
-            #region ASSIGN
-
-            TestRepository tRepo = new TestRepository();
-            CustomersController tController = null;
-
-            tController = new CustomersController(tRepo)
-            {
-                ControllerContext = UtilityFunctions.GenerateMockControllerContext("User"),
-            };
-
-            #endregion
-
-            #region ACT
-
-            var tResult = tController.Index();
-
-            #endregion
-
-            #region ASSERT
-
-            Assert.IsTrue(tResult is RedirectToActionResult);
-            Assert.AreEqual((tResult as RedirectToActionResult).ActionName, "Create");
-
-            #endregion
-        }
-
-        [TestMethod]
-        public void TestIndexGet_ExistingCustomer()
-        {
-            #region ASSIGN
-
-            TestRepository tRepo = new TestRepository();
-            CustomersController tController = null;
-
-            tController = new CustomersController(tRepo)
-            {
-                ControllerContext = UtilityFunctions.GenerateMockControllerContext("UserA"),
-            };
-
-            #endregion
-
-            #region ACT
-
-            var tResult = tController.Index() as ViewResult;
-
-            #endregion
-
-            #region ASSERT
-
-            var tValue = tResult.Model as CustomerAccountsVM;
-            Assert.AreEqual(tValue.Customer.FirstName, "John");
-            Assert.AreEqual(tValue.Accounts.Count, 2);
-
-            #endregion
-        }
-
         [TestMethod]
         public void TestEditGet_ExistingCustomer()
         {
