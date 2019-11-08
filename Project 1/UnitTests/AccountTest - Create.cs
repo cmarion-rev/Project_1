93f07cfca4b5746082b52ad5e7d78a1128291fbb
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-08 13:40:35 -0500

    Refactored out unit tests for Create Get and Post actions of AccountsController into new test class.

---
+++ Project 1/UnitTests/AccountTest - Create.cs
@@ -0,0 +1,128 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NSubstitute;
+using System;
+using System.Linq;
+using System.Web;
+using System.Security.Claims;
+
+using Data_Layer.Data_Objects;
+using Data_Layer.View_Models;
+using Web_Interface.Controllers;
+using UnitTests.Repositories;
+using Data_Layer.Resources;
+using UnitTests.Resources;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class AccountTest_Create
+    {       
+        #region CREATE
+
+        [TestMethod]
+        public void TestCreateGet()
+        {
+            #region ASSIGN
+
+            TestRepository tRepo = new TestRepository();
+            AccountsController tController = null;
+
+            tController = new AccountsController(tRepo)
+            {
+                ControllerContext = UtilityFunctions.GenerateMockControllerContext("UserA"),
+            };
+
+            #endregion
+
+            #region ACT
+
+            var tResult = tController.Create();
+
+            #endregion
+
+            #region ASSERT
+
+            Assert.IsTrue(tResult is ViewResult);
+            Assert.AreEqual(((tResult as ViewResult).ViewData["AccountTypeID"] as SelectList).Where(t=>t.Value == "0").FirstOrDefault().Text, "Checking");
+
+            #endregion
+        }
+
+        [TestMethod]
+        public void TestCreatePost_InvalidData()
+        {
+            #region ASSIGN
+
+            TestRepository tRepo = new TestRepository();
+            AccountsController tController = null;
+            Account tAccount = new Account()
+            {
+                AccountBalance = -100.0,
+            };
+
+            tController = new AccountsController(tRepo)
+            {
+                ControllerContext = UtilityFunctions.GenerateMockControllerContext("UserA"),
+            };
+
+            #endregion
+
+            #region ACT
+
+            var tResult = tController.Create(tAccount);
+
+            #endregion
+
+            #region ASSERT
+
+            Assert.IsTrue(tResult is ViewResult);
+            Assert.AreEqual(((tResult as ViewResult).Model as Account).AccountBalance, -100.0);
+
+            #endregion
+        }
+
+        [TestMethod]
+        public void TestCreatePost_ValidData()
+        {
+            #region ASSIGN
+
+            TestRepository tRepo = new TestRepository();
+            AccountsController tController = null;
+            Account tAccount = new Account()
+            {
+                AccountBalance = 100.0,
+            };
+
+            tController = new AccountsController(tRepo)
+            {
+                ControllerContext = UtilityFunctions.GenerateMockControllerContext("UserA"),
+            };
+
+            #endregion
+
+            #region ACT
+
+            var tResult = tController.Create(tAccount);
+
+            #endregion
+
+            #region ASSERT
+
+            Assert.IsTrue(tResult is RedirectToActionResult);
+            Assert.AreEqual((tResult as RedirectToActionResult).ActionName, "Details");
+            Assert.AreEqual((tResult as RedirectToActionResult).RouteValues["id"], 4);
+
+            #endregion
+        }
+
+        #endregion
+    }
+}
